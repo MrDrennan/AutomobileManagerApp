96cd8402b1893c7a697126aae4abaa6d53690b42
MrDrennan <MrDrennan@users.noreply.github.com>
2018-02-02 11:40:24 -0800

    Added Owner class

---
+++ AutomobileManagerApp/Owner.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomobileManagerApp
+{
+    /// <summary>
+    /// Represents an Owner of vehicles
+    /// </summary>
+    class Owner
+    {
+        public byte OwnerID { get; set; }
+        public string Username { get; set; }
+        public List<Vehicle> Vehicle { get; set; }
+    }
+}
