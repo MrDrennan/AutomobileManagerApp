02f569ce60f937ac681776537baf936f1e3bb871
MrDrennan <MrDrennan@users.noreply.github.com>
2018-02-02 12:37:16 -0800

    Added RepairUpgrade class

---
+++ AutomobileManagerApp/RepairUpgradeLog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomobileManagerApp
+{
+    /// <summary>
+    /// Represents repair and upgrade records of a vehicle
+    /// </summary>
+    class RepairUpgradeLog
+    {
+        public ushort RepairUpgradeLogID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        // TODO: Change this column to money data type in the database. Default to 0.
+        public double Cost { get; set; }
+        // Notes the user can write about each repair and upgrade
+        public string Notes { get; set; }
+        // Parts of each repair and upgrade that user can keep track of
+        public List<Part> Parts { get; set; }
+
+    }
+}
