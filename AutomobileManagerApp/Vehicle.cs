f7a569c049484d0b500e090111175208e15bb42d
MrDrennan <MrDrennan@users.noreply.github.com>
2018-02-02 12:02:14 -0800

    Addad Vehicle class

---
+++ AutomobileManagerApp/Vehicle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomobileManagerApp
+{
+    /// <summary>
+    /// Represents a Vehicle that can be tracked by an owner
+    /// </summary>
+    class Vehicle
+    {
+        public string VIN { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        //TODO: Make datetime only accept year component
+        public DateTime Year { get; set; }
+        public string Engine { get; set; }
+        public uint Mileage { get; set; }
+        public List<Service> Service { get; set; }
+        public List<RepairUpgradeLog> RepairUpgradeLog { get; set; }
+    }
+}
