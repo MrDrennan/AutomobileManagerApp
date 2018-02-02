5c381c7401c5c96826160f1ffe4f99de09071135
MrDrennan <MrDrennan@users.noreply.github.com>
2018-02-02 13:18:17 -0800

    Added Part Class

---
+++ AutomobileManagerApp/Part.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomobileManagerApp
+{
+    /// <summary>
+    /// Represents parts installed in a vehicle for service visit upgrade or repair
+    /// </summary>
+    class Part
+    {
+        public ushort PartID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public double Cost { get; set; }
+
+        public DateTime DatePurchased { get; set; }
+
+        public string Warranty { get; set; }
+
+        /// <summary>
+        /// Notes about each part for the user
+        /// </summary>
+        public string Notes { get; set; }
+    }
+}
